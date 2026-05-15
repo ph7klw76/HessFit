c67583f4d41c6b8a16afd9985f6c6444a70e729d
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2026-05-15 16:24:56 +0800

    Add files via upload

---
+++ a12.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+ 
+#SBATCH -p cpu-epyc-genoa
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 2
+#SBATCH --mem 8G
+#SBATCH -J LBAI
+#SBATCH --time=0-00:59:59
+
+
+module load miniconda/24.1.2
+
+source activate /home/user/woon/ML
+
+python update_itp_full_pipeline.py \
+  --data ./data \
+  --base-itp BK7T.itp \
+  --dihe-json dihe_optfile.json \
+  --charges type_charge.txt \
+  --ff-string ff_string.txt \
+  --output-itp BK7T_full_hessfit_scan_refined_relaxed.itp \
+  --allow-incomplete \
+  --expected-points 7 \
+  --allow-high-rmse
